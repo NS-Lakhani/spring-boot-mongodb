7cf3d72f4edc640d16ed29a7b0a2229ff094ef89
Nishat <lakhani.nishat1992@gmail.com>
2022-08-17 12:58:11 +0530

    Added @Id annotation on Photo Collection

--- src/main/java/com/javachamp/collection/Photo.java
+++ src/main/java/com/javachamp/collection/Photo.java
@@ -1,6 +1,7 @@
 package com.javachamp.collection;
 
 import org.bson.types.Binary;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.Data;
 @Document(collection = "photo")
 public class Photo {
 	
+	@Id
 	private String id;
 	private String name;
 	private Binary photo;
